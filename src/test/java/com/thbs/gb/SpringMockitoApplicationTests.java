abcbe44b0a58721677c1e438ef61ef5de60ef11d
Chetan Raj Jayanna <chetanraj_jayanna@thbs.india.com>
2019-07-29 18:22:42 +0530

    Added unit test cases for all the 3 layers(Model,View,Controller)

--- src/test/java/com/thbs/gb/SpringMockitoApplicationTests.java
+++ src/test/java/com/thbs/gb/SpringMockitoApplicationTests.java
@@ -1,9 +1,13 @@
 package com.thbs.gb;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,17 +15,22 @@ import java.util.stream.Stream;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.thbs.gb.model.Entry;
 import com.thbs.gb.repository.EntryRepository;
 import com.thbs.gb.repository.UserRepository;
 import com.thbs.gb.service.EntryService;
+import com.thbs.gb.web.EntryController;
+import com.thbs.gb.web.UserController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringMockitoApplicationTests {
 
 	@Autowired
@@ -32,7 +41,26 @@ public class SpringMockitoApplicationTests {
 
 	@MockBean
 	private UserRepository mockedUserRepository;
+	
+	@Autowired
+    private UserController userController;
+	
+	@Autowired
+    private EntryController entryController;
+	
+	@Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void entryControllerContexLoads() throws Exception {
+        assertThat(entryController).isNotNull();
+    }
+
+    @Test
+    public void userControllerContexLoads() throws Exception {
+        assertThat(userController).isNotNull();
+    }
+    
 	@Test
 	public void getAllEntriesTest() {
 
@@ -60,7 +88,7 @@ public class SpringMockitoApplicationTests {
 	}
 
 	@Test
-	public void saveOrUpdateTest() {
+	public void saveOrUpdateEntryTest() {
 
 		Entry entry = new Entry();
 		entry.setTextMessage("SampleMessage");
@@ -70,4 +98,13 @@ public class SpringMockitoApplicationTests {
 		verify(mockedEntryRepository, times(1)).save(entry);
 
 	}
+	
+	@Test
+    public void testMVC() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().is3xxRedirection());
+        this.mockMvc.perform(get("/entry")).andDo(print()).andExpect(status().is3xxRedirection());
+        this.mockMvc.perform(get("/adminPanel")).andDo(print()).andExpect(status().is3xxRedirection());
+        this.mockMvc.perform(get("/login")).andDo(print()).andExpect(status().isOk());
+    }
+	
 }
